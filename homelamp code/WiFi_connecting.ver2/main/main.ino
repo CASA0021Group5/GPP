b2584762426bfd10874c745eba3677f87e2dc3f3
MCH0202 <ucfnmh1@ucl.ac.uk>
2025-03-03 14:38:50 +0000

    Create main Arduino folder

---
+++ homelamp code/WiFi_connecting.ver2/main/main.ino
@@ -0,0 +1,32 @@
+#include "WiFiUser.h"
+
+const int resetPin = 0;               // Reset button pin, used to clear WiFi credentials
+int connectTimeOut_s = 15;            // WiFi connection timeout (seconds)
+
+void setup() 
+{
+  pinMode(resetPin, INPUT_PULLUP);    // Set button as input with pull-up (default HIGH, pulled LOW when pressed)
+  Serial.begin(115200);               // Set baud rate
+  
+  wifiConfig();  // Start WiFi configuration mode (AP mode for setup or STA mode for connection)
+}
+
+void loop() 
+{
+  if (!digitalRead(resetPin)) // Press and hold for 5 seconds to clear WiFi credentials
+  {
+    delay(5000);  // This method is not precise
+    if (!digitalRead(resetPin)) 
+    {
+      Serial.println("\nButton held for 5 seconds, clearing saved WiFi credentials.");
+      restoreWiFi();  // Erase stored WiFi credentials
+      ESP.restart();  // Restart ESP32
+      Serial.println("Device restarting..."); // This might not be printed before the restart
+    }
+  }
+  
+  checkDNS_HTTP();   // Process DNS & HTTP client requests (for captive portal)
+  checkConnect(true);  // Check WiFi connection status, reconnect if disconnected
+
+  delay(30);
+}
